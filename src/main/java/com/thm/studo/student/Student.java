c2b4888edf2f9253a45e5fd59e43407ff9397f03
AmarJ92 <Amar.Javid@web.de>
2023-01-15 17:58:35 +0100

    fixed imports for javax to jakarta

--- src/main/java/com/thm/studo/student/Student.java
+++ src/main/java/com/thm/studo/student/Student.java
@@ -1,11 +1,10 @@
 package com.thm.studo.student;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Entity(name = "Student")
