4d41212be6783f0e99ea9b6eee7e152e7463a478
AmarJ92 <Amar.Javid@web.de>
2023-03-31 21:50:40 +0200

    some stuff for spring security

---
+++ src/main/java/com/thm/studo/auth/AuthenticationService.java
@@ -0,0 +1,12 @@
+package com.thm.studo.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+
+
+}
